f28aecbc25221ea1a5e4f37547d8964954d8ae65
Anastasia <sawiii@mail.ru>
2022-08-29 11:29:43 +0300

    Second task

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит третью цифру случайно 
+// сгенерированного числа (от 1 до 65536) или сообщает, что 
+// третьей цифры нет.
+Random rand = new Random();
+int x = rand.Next(65536 + 1); // случайное число
+
+if (x > 99)
+{
+    Console.WriteLine(x);
+    while (x > 999)
+    {
+        x /= 10;
+        //Console.WriteLine(x);
+    }
+    int z = x / 100; // первая цифра
+    int y = (x - z * 100) % 10; // третья цифра
+        
+    //Console.WriteLine(z);
+    Console.WriteLine(y); 
+}
+else
+{
+   Console.WriteLine("Третьей цифры нет"); 
+}
+
